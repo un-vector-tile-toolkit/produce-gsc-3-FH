8a3a36d9ba62174198e0caba58aa7f5c3d5db290
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-05-03 15:08:08 -0400

    rev

--- produce-gsc-small/modify-old0503.js
+++ produce-gsc-small/modify-old0503.js
@@ -310,13 +310,14 @@ f.properties.display = 1
     let popp_arr = [1, 2, 3]
    if (f.properties.cartolb === 'Alofi' ||f.properties.cartolb === 'Avarua' ||f.properties.cartolb === 'Sri Jayewardenepura Kotte' ) {
      return null
-    } else if (f.properties.poptyp_code == 1 || f.properties.poptyp_code == 2) {
-     return f
-    } else if (f.properties.poptyp_code == 3 && f.properties.scl_id_code == 10) {
-     return f
-    } else {
+    } else if (!popp_arr.includes(f.properties.poptyp_code)) {
      return null
-    } 
+    }
+    if (f.properties.poptyp_code !== 3 || f.properties.scl_id_code !== 10) {
+     return null
+    } else {
+    return f
+    }
   } 
 }
 module.exports = (f) => {
